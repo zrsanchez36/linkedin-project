7799d6e1ea86599a8a4ef3dfd695244ca618ba1d
Zsoreign <zrsanchez36@gmail.com>
2023-06-10 11:55:58 -0400

    UpDATE USER 1

---
+++ Scripts/index.js
@@ -0,0 +1,18 @@
+// Foldernames should be : Kebab-Case
+//js variables should be : camelCase
+// Commits should : Kebab case
+
+
+let userName = 'Zsoreign Sanchez';
+let profilePicture = "./assets/images/man.png"
+let jobTitle = 'Software Engineer'
+
+// DOM Steps
+// Step 1: Target element
+// Step 2: Store element in a temporary Variable
+// Step 3: ADD/ Remove/ update the element
+
+let usernameElement: document.querySelector(".data-username");
+usernameElement.innerText = userName;
+
+
